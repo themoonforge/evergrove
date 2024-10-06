60ba3040e4d7ac0630d0ad12048d2fc78347935f
Stephan Mielke <bognari.s@gmail.com>
2024-10-06 15:33:38 +0200

    refactor_behaviour

--- evergrove/Utils.gd
+++ evergrove/Utils.gd
@@ -6,3 +6,20 @@ enum BuildingType{
 
 const TILE_SIZE_HALF = 8
 const TILE_SIZE = TILE_SIZE_HALF * 2
+
+enum WalkingDirection {
+	FRONT,
+	BACK,
+	LEFT,
+	RIGHT,
+	DEFAULT
+}
+
+enum Behaviour {
+	IDLE,
+	WALKING,
+	SWIMMING,
+	MINING,
+	SLEEPING,
+	EATING,
+}
--- evergrove/dwarf/dwarf.gd
+++ evergrove/dwarf/dwarf.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 class_name Dwarf
 
+const Utils = preload("../Utils.gd")
+
 @export var current_dungeon_layer: DungeonLayer
 
 @onready var character_animator: AnimatedSprite2D = $"./CharacterAnimator"
@@ -11,8 +13,8 @@ class_name Dwarf
 @onready var world: World = $"/root/Game/World"
 
 @export var walking_speed: float = 75.0
-@export var walking_direction: WalkingDirection = WalkingDirection.DEFAULT
-@export var behaviour: Behaviour = Behaviour.IDLE
+@export var walking_direction: Utils.WalkingDirection = Utils.WalkingDirection.DEFAULT
+@export var behaviour: Utils.Behaviour = Utils.Behaviour.IDLE
 @export var walking_path: PackedVector3Array = []
 @export var current_level: int = 0
 @export var current_position: Vector2i = Vector2i(0, 0)
@@ -96,7 +98,7 @@ func _ready():
 	add_child(Agent.create())
 	game_state.selected_dwarf = self
 	game_state.inc_dwarfs(1)
-	set_animation(Behaviour.IDLE, WalkingDirection.DEFAULT)
+	set_animation(Utils.Behaviour.IDLE, Utils.WalkingDirection.DEFAULT)
 
 func play_animation(animator: AnimatedSprite2D, animation: String) -> void:
 	if !animator.is_playing() || animator.animation != animation || !animator.visible:
@@ -108,7 +110,7 @@ func stop_animation(animator: AnimatedSprite2D) -> void:
 		animator.stop()
 		animator.visible = false
 
-func set_animation(my_behaviour: Behaviour, my_walking_direction: WalkingDirection) -> void:
+func set_animation(my_behaviour: Utils.Behaviour, my_walking_direction: Utils.WalkingDirection) -> void:
 	if behaviour == my_behaviour && walking_direction == my_walking_direction:
 		return	
 
@@ -116,60 +118,60 @@ func set_animation(my_behaviour: Behaviour, my_walking_direction: WalkingDirecti
 	walking_direction = my_walking_direction
 	
 	match behaviour:
-		Behaviour.IDLE:
+		Utils.Behaviour.IDLE:
 			play_animation(character_animator, idle_animation)
 			stop_animation(effect_animator)
-		Behaviour.WALKING:
+		Utils.Behaviour.WALKING:
 			stop_animation(effect_animator)
 			match walking_direction:
-				WalkingDirection.FRONT:
+				Utils.WalkingDirection.FRONT:
 					play_animation(character_animator, walking_front_animation)
-				WalkingDirection.BACK:
+				Utils.WalkingDirection.BACK:
 					play_animation(character_animator, walking_back_animation)
-				WalkingDirection.LEFT:
+				Utils.WalkingDirection.LEFT:
 					play_animation(character_animator, walking_left_animation)
-				WalkingDirection.RIGHT:
+				Utils.WalkingDirection.RIGHT:
 					play_animation(character_animator, walking_right_animation)
-				WalkingDirection.DEFAULT:
+				Utils.WalkingDirection.DEFAULT:
 					play_animation(character_animator, walking_default_animation)
-		Behaviour.SWIMMING:
+		Utils.Behaviour.SWIMMING:
 			stop_animation(effect_animator)
 			match walking_direction:
-				WalkingDirection.FRONT:
+				Utils.WalkingDirection.FRONT:
 					play_animation(character_animator, swiming_front_animation)
-				WalkingDirection.BACK:
+				Utils.WalkingDirection.BACK:
 					play_animation(character_animator, swiming_back_animation)
-				WalkingDirection.LEFT:
+				Utils.WalkingDirection.LEFT:
 					play_animation(character_animator, swiming_left_animation)
-				WalkingDirection.RIGHT:
+				Utils.WalkingDirection.RIGHT:
 					play_animation(character_animator, swiming_right_animation)
-				WalkingDirection.DEFAULT:
+				Utils.WalkingDirection.DEFAULT:
 					play_animation(character_animator, swiming_default_animation)
-		Behaviour.MINING:
+		Utils.Behaviour.MINING:
 			play_animation(effect_animator, mining_effect)
 			match walking_direction:
-				WalkingDirection.FRONT:
+				Utils.WalkingDirection.FRONT:
 					play_animation(character_animator, mining_front_animation)
-				WalkingDirection.BACK:
+				Utils.WalkingDirection.BACK:
 					play_animation(character_animator, mining_back_animation)
-				WalkingDirection.LEFT:
+				Utils.WalkingDirection.LEFT:
 					play_animation(character_animator, mining_left_animation)
-				WalkingDirection.RIGHT:
+				Utils.WalkingDirection.RIGHT:
 					play_animation(character_animator, mining_right_animation)
-				WalkingDirection.DEFAULT:
+				Utils.WalkingDirection.DEFAULT:
 					play_animation(character_animator, mining_default_animation)
-		Behaviour.SLEEPING:
+		Utils.Behaviour.SLEEPING:
 			play_animation(effect_animator, sleeping_effect)
 			play_animation(character_animator, sleeping_animation)
-		Behaviour.EATING:
+		Utils.Behaviour.EATING:
 			play_animation(effect_animator, eating_effect)
 			play_animation(character_animator, eating_animation)
 
 
-func set_behaviour(my_behaviour: Behaviour) -> void:
+func set_behaviour(my_behaviour: Utils.Behaviour) -> void:
 	set_animation(my_behaviour, walking_direction)
 
-func set_walking_direction(my_direction: WalkingDirection) -> void:
+func set_walking_direction(my_direction: Utils.WalkingDirection) -> void:
 	set_animation(behaviour, my_direction)
 
 func _process(delta):
@@ -183,7 +185,7 @@ func _process(delta):
 			current_dungeon_layer.dwarf_container.add_child(self)
 			walking_path.remove_at(0)
 			if walking_path.size() == 0:
-				set_animation(Behaviour.IDLE, WalkingDirection.DEFAULT)
+				set_animation(Utils.Behaviour.IDLE, Utils.WalkingDirection.DEFAULT)
 			return
 		var target2D = world.visible_tile_map.map_to_local(Vector2(target.x, target.y))
 
@@ -192,13 +194,13 @@ func _process(delta):
 		
 		var offset = Vector2i(0, 0)
 		match walking_direction:
-			WalkingDirection.FRONT:
+			Utils.WalkingDirection.FRONT:
 				offset.y = 1
-			WalkingDirection.BACK:
+			Utils.WalkingDirection.BACK:
 				offset.y = -1
-			WalkingDirection.LEFT:
+			Utils.WalkingDirection.LEFT:
 				offset.x = -1
-			WalkingDirection.RIGHT:
+			Utils.WalkingDirection.RIGHT:
 				offset.x = 1
 
 		var next_position = current_position + offset
@@ -209,21 +211,21 @@ func _process(delta):
 		var is_walkable_next: float = data.get_custom_data("is_walkable")
 		var is_water_next: float = data.get_custom_data("is_water")
 
-		var my_behaviour = Behaviour.WALKING
+		var my_behaviour = Utils.Behaviour.WALKING
 
 		if is_water_next:
-			my_behaviour = Behaviour.SWIMMING
+			my_behaviour = Utils.Behaviour.SWIMMING
 		if !is_walkable_next:
-			my_behaviour = Behaviour.MINING
+			my_behaviour = Utils.Behaviour.MINING
 
 		if direction.x > 0:
-			set_animation(my_behaviour, WalkingDirection.RIGHT)
+			set_animation(my_behaviour, Utils.WalkingDirection.RIGHT)
 		elif direction.x < 0:
-			set_animation(my_behaviour, WalkingDirection.LEFT)
+			set_animation(my_behaviour, Utils.WalkingDirection.LEFT)
 		elif direction.y > 0:
-			set_animation(my_behaviour, WalkingDirection.FRONT)
+			set_animation(my_behaviour, Utils.WalkingDirection.FRONT)
 		elif direction.y < 0:
-			set_animation(my_behaviour, WalkingDirection.BACK)
+			set_animation(my_behaviour, Utils.WalkingDirection.BACK)
 
 		velocity *= walking_speed_factor_next
 
@@ -240,9 +242,12 @@ func _process(delta):
 				world.mine_tile(current_position, current_level)
 		
 			if walking_path.size() == 0:
-				set_animation(Behaviour.IDLE, WalkingDirection.DEFAULT)
+				set_animation(Utils.Behaviour.IDLE, Utils.WalkingDirection.DEFAULT)
 
 func walk_to(target: Vector2, level: int) -> void:
+	print("walk_to")
+	if (walking_path.size() > 0):
+		print("Already walking")
 	var current_key = world.get_unique_id(current_position, current_level)
 	var target_key = world.get_unique_id(target, level)
 	var path = world.astar.get_point_path(current_key, target_key)
