bfe1474407cfb8678321411e7cdcb5762d393e24
DC4JG <github@jakobgarbe.de>
2024-10-06 16:54:09 +0200

    feat: class wrapper for 2D coordinates and map layer

--- evergrove/AI/ai_globals.gd
+++ evergrove/AI/ai_globals.gd
@@ -11,6 +11,7 @@ signal AGENT_CREATED(agent: Agent)
 signal PROCESS_TICK()
 signal AGENT_WITHOUT_TASK(agent:Agent)
 signal AGENT_NO_LONGER_TASKLESS(agent:Agent)
+signal ENERGY_HUB_SPAWNED(energy_hub:EnergyHub)
 
 enum TASK_TYPE {
 	MOVE_TO,
@@ -18,3 +19,12 @@ enum TASK_TYPE {
 	EAT,
 	SLEEP
 }
+
+class Location:
+	var layer:int
+	var coordinates:Vector2i
+	static func create(coordinates:Vector2i, layer:int) -> Location:
+		var instance = Location.new()
+		instance.coordinates = coordinates
+		instance.layer - layer
+		return instance
